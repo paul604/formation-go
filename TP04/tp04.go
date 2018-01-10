5b1b6b6570a4b6aca6caa20f45773ce1c9c3074d
paul <paul604@users.noreply.github.com>
2018-01-10 11:38:34 +0100

    fin TP04

--- TP04/tp04.go
+++ TP04/tp04.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"time"
+        "fmt"
+        "time"
 )
 
 var weekdays = []string{"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
 }
 
-func incr(i int) {
-	i = i + 1
+func incr(i *int) {
+        *i = *i + 1
 }
 
 func main() {
-	i := int(time.Now().Weekday())
-	fmt.Printf("Today is %s\n", weekdays[i])
-	incr(i)
-	fmt.Printf("Tomorrow is %s\n", weekdays[i])
+        i := int(time.Now().Weekday())
+        fmt.Printf("Today is %s\n", weekdays[i])
+        incr(&i)
+        fmt.Printf("Tomorrow is %s\n", weekdays[i])
 }
